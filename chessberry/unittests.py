42941dc2860c0e84eaed6d26a7f11e91fdf6d5a2
Espen Nordahl <espen.nordah@gmail.com>
2016-05-21 13:48:25 +0200

    Mocking up chess API with unit tests

---
+++ chessberry/unittests.py
@@ -0,0 +1,23 @@
+import unittest
+
+from chess import Game, Move, Color
+
+class TestMoveLogic(unittest.TestCase):
+	def setUp(self):
+		self.game = Game()
+
+	def testWhiteBasicMove(self):
+		newMove = Move(Color.white, 'd4')
+		self.assertTrue(newMove.isLegal())
+		self.assertTrue(self.game.isMoveLegal(newMove))
+		self.assertTrue(self.game.applyMove(newMove))
+	
+	def testWhiteMoveOutsideOfBoard(self):
+		newMove = Move(Color.white, 'm4')
+		self.assertFalse(newMove.isLegal())
+		self.assertFalse(self.game.isMoveLegal(newMove))
+		self.assertFalse(self.game.applyMove(newMove))
+
+
+if __name__ == '__main__':
+	unittest.main()
